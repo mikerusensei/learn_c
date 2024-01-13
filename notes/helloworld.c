7e32607fe7d4df949046b8e6d1b407d6615b1ed5
Michael Ponce <michaelalexisponce15@gmail.com>
2024-01-13 19:06:37 +0800

    :rocket: Hello World

---
+++ notes/helloworld.c
@@ -0,0 +1,19 @@
+// Hello World Program using C
+// January 13, 2024
+// by: mikerusensei
+
+// Header file for input and output functions
+#include <stdio.h>
+
+// main function
+// Where the execution of the program begins
+int main(void){
+
+    // Prints Hello World
+    printf("Hello World");
+
+    return 0;
+}
+
+// Output
+// Hello World
