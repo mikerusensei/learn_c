db71aff0b337bfee9da0b7b6c21de765e15bda24
Michael Ponce <michaelalexisponce15@gmail.com>
2024-01-13 19:07:19 +0800

    :rocket: First Program

---
+++ notes/firstprog.c
@@ -0,0 +1,14 @@
+// First program using C
+// January 13, 2024
+// by: mikerusensei
+
+#include <stdio.h>          // Header
+
+int main(){                 // Main [int main(void)]
+    int a = 10;             // Statements
+    printf("%d\n", a);      // Statements
+    return 0;               // Return
+}
+
+// Output
+// 10
